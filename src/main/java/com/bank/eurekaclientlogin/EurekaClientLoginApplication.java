06cc6512b590972833d7388dcf0643f2d3e7997f
Vikas_Kaushik <Vikas_Kaushik@EPINPUNW0218>
2021-07-29 15:58:08 +0530

    Added base class

--- src/main/java/com/bank/eurekaclientlogin/EurekaClientLoginApplication.java
+++ src/main/java/com/bank/eurekaclientlogin/EurekaClientLoginApplication.java
@@ -2,10 +2,19 @@ package com.bank.eurekaclientlogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class EurekaClientLoginApplication {
 
+	@RequestMapping("/")
+	public String home() {
+		return "Hello eureka-client-login";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientLoginApplication.class, args);
 	}
---
+++ src/main/java/com/bank/eurekaclientlogin/helper/constants/IRestConstants.java
@@ -0,0 +1,15 @@
+package com.bank.eurekaclientlogin.helper.constants;
+
+/**
+ * @author
+ *
+ */
+public interface IRestConstants {
+
+	// URL Constants
+	String SEPARATOR = "/";
+	String API_BASE = "api";
+	String OPEN_BRACKET = "{";
+	String CLOSE_BRACKET = "}";
+	String ID = "id";
+}
