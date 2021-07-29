ccf5d1b81ef8a3210793d82ec8272a405a7f8e46
Vikas Kaushik <vikas_kaushik@epam.com>
2021-07-29 11:27:10 +0530

    Initial commit

---
+++ src/main/java/com/bank/eurekaclientlogin/EurekaClientLoginApplication.java
@@ -0,0 +1,13 @@
+package com.bank.eurekaclientlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/eurekaclientlogin/EurekaClientLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.eurekaclientlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
